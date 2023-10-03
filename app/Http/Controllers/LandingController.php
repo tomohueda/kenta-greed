6ce146656df48f6ee227c48454b7e35bc8120c75
Tomoh Ueda <tomohueda@gmail.com>
2023-10-03 19:34:27 +0900

    開始時間修正

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class LandingController extends Controller
 {
     public function show(){
-        $startTime = "October 8, 2023 12:00:00 UTC+0900";
+        $startTime = "October 8, 2022 12:00:00 UTC+0900";
         //$startTime = "April 12, 2023 09:00:00 UTC+0900";
         return view (
             //'top',
