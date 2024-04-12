12045b1ba5a6d6c7ca8b5b3b3a2fdc8f0e315042
Tomoh Ueda <tomohueda@gmail.com>
2024-04-13 07:47:12 +0900

    一部修正

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -45,7 +45,7 @@
 				<!--<h1>Coming soon!</h1> -->
 				
 				<!-- Sub header -->
-				<p class="lead" style="margin-top:50px; line-height: 1.5em;">KENTA MORIMOTO TRIO<br>presents</p>
+				<p class="lead" style="margin-top:50px; line-height: 1.5em;">KENTA MORIMOTO</p>
 				
 				<!-- Countdown -->
 				<div class="countdown circled large" style="margin-top:0px">
